2972f76e86d464cceeafc918b6060f57b2715b2d
Christian Guetnga <lsdrfrx@gmail.com>
2024-01-12 17:09:08 +0300

    added a lot of plugins, refactoring

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("57f96b03e3abb26948c336e0577439edb7b879e89368406736dcbce812fef327" "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" default))
+   '("80214de566132bf2c844b9dee3ec0599f65c5a1f2d6ff21a2c8309e6e70f9242" "57f96b03e3abb26948c336e0577439edb7b879e89368406736dcbce812fef327" "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" default))
  '(org-link-frame-setup
    '((vm . vm-visit-folder-other-frame)
      (vm-imap . vm-visit-imap-folder-other-frame)
@@ -16,7 +16,7 @@
      (file . find-file)
      (wl . wl-other-frame)))
  '(package-selected-packages
-   '(olivetti zen-mode doom-themes company lsp-ui org-roam-ui dashboard seq org-roam lsp-pyright lsp-mode treemacs-tab-bar treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs use-package)))
+   '(beacon 2048-game reverse-im helm olivetti zen-mode doom-themes company lsp-ui org-roam-ui dashboard seq org-roam lsp-pyright lsp-mode treemacs-tab-bar treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
