1b59d29a25fd94e18bec895bd6bcfb6932389734
Christian Guetnga <lsdrfrx@gmail.com>
2024-01-12 08:00:05 +0300

    added some plugins, config refactored

--- init.el
+++ init.el
@@ -2,3 +2,24 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("57f96b03e3abb26948c336e0577439edb7b879e89368406736dcbce812fef327" "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" default))
+ '(org-link-frame-setup
+   '((vm . vm-visit-folder-other-frame)
+     (vm-imap . vm-visit-imap-folder-other-frame)
+     (gnus . org-gnus-no-new-news)
+     (file . find-file)
+     (wl . wl-other-frame)))
+ '(package-selected-packages
+   '(olivetti zen-mode doom-themes company lsp-ui org-roam-ui dashboard seq org-roam lsp-pyright lsp-mode treemacs-tab-bar treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
